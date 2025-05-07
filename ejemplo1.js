8c413996686cb10b1a1e860a1a58dcd1c327aa63
Ximena M <ximenaa4455@gmail.com>
2025-05-06 18:51:22 -0600

    modifique variables

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,14 +1,10 @@
 // este es un comentario lineal en JavaScript
 // inicialización de variables
-var nombre = "Juan";
-
-var edad = "23";
-
-var estudiante = true;
-
-var sueldo;
-
-var direccion = null;
+var nombre = "Juan"; //tipo string
+var edad = "23"; //tipo number
+var estudiante = true; //tipo boolean
+var sueldo; //tipo undefined (indefinido)
+var direccion = null; //tipo null
 
 // imprimir variables
 console.log("Nombre:", nombre);
@@ -16,7 +12,9 @@ console.log("Edad:" + edad);
 console.log("Estudiante:"+ estudiante);
 console.log("Sueldo:", sueldo);
 console.log("Direccion:", direccion);
-// ahora procederemos a realizar un ejemplo utilizando typeof, esto que esta viendo es un comentario multi linea o bloque //
+
+// ahora procederemos a realizar un ejemplo utilizando typeof
+// esto que esta viendo es un comentario multi linea o bloque //
 
 console.log ("Nombre:", typeof nombre);
 console.log ("Edad:" + typeof edad);
